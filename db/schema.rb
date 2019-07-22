4c67c889bafca1df83ff027abfc259df72f9bca8
user <user@usernoMacBook-Pro.local>
2019-07-22 20:35:36 +0900

    Taskモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_20_075616) do
+ActiveRecord::Schema.define(version: 2019_07_22_113335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
