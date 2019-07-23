d3060088984cab2cf60dd6e24db755e7e70ff6f1
user <user@usernoMacBook-Pro.local>
2019-07-23 19:56:51 +0900

    ステップ６

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_20_075616) do
+ActiveRecord::Schema.define(version: 2019_07_23_105615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
-    t.string "content"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
