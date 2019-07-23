8d3e9c7057190080a053a4685417479b5a73085b
user <user@usernoMacBook-Pro.local>
2019-07-23 21:06:25 +0900

    TaskモデルCRUD作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'task/index'
-  get 'task/new'
-  get 'task/create'
-  get 'task/edit'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "tasks#index"
+  resources :tasks
 end
