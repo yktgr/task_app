78cfcdaab4d6fcc6aba5613cb241e6b2fe9175ae
user <user@usernoMacBook-Pro.local>
2019-07-27 13:49:05 +0900

    show view日本語化

--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -26,3 +26,25 @@ RSpec.feature "タスク管理機能", type: :feature do
     expect(page).to have_content 'content_test'
   end
 end
+
+
+RSpec.feature "タスク管理機能", type: :feature do
+  background do
+    Task.create!(title:)
+  end
+  scenario "タスク一覧のテスト" do
+    # タスク一覧のテスト
+  end
+
+  scenario "タスク作成のテスト" do
+    # タスク作成のテスト
+  end
+
+  scenario "タスク詳細のテスト" do
+    # タスク詳細のテスト
+  end
+
+  scenario "タスクが作成日時の降順に並んでいるかのテスト" do
+    # ここにテスト内容を記載する
+  end
+endß
