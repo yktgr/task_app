34776b90cf4ac4ff52b96ee088237c27cb15e841
user <user@usernoMacBook-Pro.local>
2019-07-28 22:26:44 +0900

    終了期限を追加

---
+++ db/migrate/20190728093035_add_column_expired_at.rb
@@ -0,0 +1,5 @@
+class AddColumnExpiredAt < ActiveRecord::Migration[5.2]
+  def change
+    add_column:tasks,:expired_at,:datetime, null:false
+  end
+end
--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -9,29 +9,39 @@ RSpec.feature "タスク管理機能", type: :feature do
     visit root_path
     expect(page).to have_content 'Factoryタイトル1'
     expect(page).to have_content 'Factoryコンテント1'
+    expect(page).to have_content '08-01'
     expect(page).to have_content 'Factoryタイトル2'
     expect(page).to have_content 'Factoryコンテント2'
+    expect(page).to have_content '12-01'
   end
 
   scenario "タスク作成のテスト" do
     visit new_task_path
     fill_in 'タスク名', with: 'title_test'
     fill_in '内容', with: 'content_test'
+    fill_in '終了期限', with: '1/31'
     click_button '登録する'
     expect(page).to have_content 'content_test'
+
   end
 
   scenario "タスク詳細のテスト" do
     visit root_path
-    all('div')[1].click_link '詳細'
-    expect(page).to have_content 'Factoryコンテント1'
-    visit root_path
-    all('div')[0].click_link '詳細'
+    all('tr')[1].click_link '詳細'
     expect(page).to have_content 'Factoryコンテント2'
+    visit root_path
+    all('tr')[2].click_link '詳細'
+    expect(page).to have_content 'Factoryコンテント1'
   end
 
   scenario "タスクが作成日時の降順に並んでいるかのテスト" do
     visit root_path
      expect(Task.order("created_at DESC").map(&:id)).to eq [9,8]
+  end
+
+  scenario "タスクが終了期限の降順に並んでいるかのテスト" do
+    visit root_path
+    click_link "終了期限でソートする"
+    expect(Task.order("expired_at DESC").map(&:id)).to eq [11,10]
   end
 end
